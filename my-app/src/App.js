dd6de0faf7df06eae8cbc61374b93ebc379ba16d
Andrey Smirnov <aosmirnov_4@edu.hse.ru>
2024-02-06 20:02:19 +0500

    test2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="panel">
       <Header />
-      <h2>Взял на чек Номер 2</h2>
+      <h2>Взял на чек VVVV</h2>
+      <h2>тест</h2>
+      
       
     </div>
   );
