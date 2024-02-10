77350dade7380becc008d8513f1546d9cca126da
Andrey Smirnov <aosmirnov_4@edu.hse.ru>
2024-02-11 03:19:47 +0500

    new icons

--- my-app/src/components/contentList.js
+++ my-app/src/components/contentList.js
@@ -7,6 +7,11 @@ function ContentList({ dataList }) {
         <div className='head'>
             <div className='notice'>
                 <h1 className='title'>Название страницы</h1>
+                <div className='navigation'>
+                <img width="32" height="32" src="https://img.icons8.com/fluency-systems-regular/48/grid.png" alt="grid"/>
+                <img width="32" height="32" src="https://img.icons8.com/fluency-systems-regular/48/grid-2.png" alt="grid-2"/>
+                <img width="32" height="32" src="https://img.icons8.com/fluency-systems-regular/48/grid-3.png" alt="grid-3"/>
+                </div>
                 <notice>Описание страницы</notice>
             </div>
             <div className='Content-container'>
