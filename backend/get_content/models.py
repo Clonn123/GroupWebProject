acc7605a3cc99534582498f67e0504a9b773f354
Poloznikov Artem <art-clon@mail.ru>
2024-02-20 19:56:37 +0500

    Добавил авторизацию через БД и прочее

--- backend/get_content/models.py
+++ backend/get_content/models.py
@@ -4,3 +4,17 @@ class Animes(models.Model):
     url_img = models.TextField()
     title = models.TextField()
     description = models.TextField()
+
+    class Meta:
+        db_table = 'animes'
+
+class Users(models.Model):
+    name = models.TextField()
+    surname = models.TextField()
+    username = models.TextField()
+    password = models.TextField()
+    email = models.TextField()
+
+    class Meta:
+        db_table = 'users'
+
