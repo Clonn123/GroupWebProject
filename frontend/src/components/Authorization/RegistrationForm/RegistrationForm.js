569a440a5ad7b9da143f18ddc26a8a17ca400f45
Poloznikov Artem <art-clon@mail.ru>
2024-02-28 21:56:02 +0500

    Возвращаем утраченное 8

--- frontend/src/components/Authorization/RegistrationForm/RegistrationForm.js
+++ frontend/src/components/Authorization/RegistrationForm/RegistrationForm.js
@@ -12,6 +12,7 @@ function RegistrationForm() {
     const [password, setPassword] = useState('');
     const [gender, setGender] = useState('other');
     const [age, setAge] = useState('');
+    const [birthdate, setBirthdate] = useState('');
     const [error, setError] = useState('');
     const [successMessage, setSuccessMessage] = useState('');
     const [showModal, setShowModal] = useState(false); // Состояние для отслеживания видимости модального окна
@@ -20,7 +21,7 @@ function RegistrationForm() {
     const handleSubmit = async (event) => {
       event.preventDefault(); // Предотвращаем стандартное поведение отправки формы
 
-      if (!email || !name || !surname || !username || !password || !gender || !age) {
+      if (!email || !name || !surname || !username || !password || !gender || !birthdate) {
         setError('Пожалуйста, заполните все поля');
         return;
       }
@@ -33,7 +34,7 @@ function RegistrationForm() {
           username,
           password,
           gender,
-          age
+          birthdate
       }).then((response) => {
         
       // Успешно зарегистрирован
@@ -92,15 +93,21 @@ function RegistrationForm() {
               value={gender}
               onChange={(e) => setGender(e.target.value)}
           >
-              <option value="other">Другой</option>
-              <option value="male">Мужской</option>
-              <option value="female">Женский</option>              
+              <option value="Другой">Другой</option>
+              <option value="Мужской">Мужской</option>
+              <option value="Женский">Женский</option>              
           </select>
-          <input
+          {/* <input
               type="number"
               placeholder="Возраст"
               value={age}
               onChange={(e) => setAge(e.target.value)}
+          /> */}
+          <input
+              type="date"
+              placeholder="Дата рождения"
+              value={birthdate}
+              onChange={(e) => setBirthdate(e.target.value)}
           />
           <button type="submit">Зарегистрироваться</button>
           {error && <p className="error-message">{error}</p>}
