bea056b3d7db8007803024b6ff20e94a29d1e09e
Andrey Smirnov <aosmirnov_4@edu.hse.ru>
2024-03-08 20:35:44 +0500

    plox?

--- frontend/src/components/Contents/PageContent/PageContent.js
+++ frontend/src/components/Contents/PageContent/PageContent.js
@@ -1,49 +1,60 @@
-import './PageContent.css';
-import { useParams } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import RatingComponent from '../SettintContent/RatingComponent.js'
-import StatusComponent from '../SettintContent/StatusComponent.js'
+import "./PageContent.css";
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import RatingComponent from "../SettintContent/RatingComponent.js";
+import StatusComponent from "../SettintContent/StatusComponent.js";
+import plox from './plox.png';
 
 const PageContent = () => {
   const { id } = useParams();
   const [infoList, setInfoList] = useState();
   useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/info/anime/'+id)
-      .then(response => {
+    axios
+      .get("http://127.0.0.1:8000/api/info/anime/" + id)
+      .then((response) => {
         setInfoList(response.data);
       })
-      .catch(error => {
-        console.error('Ошибка:', error);
+      .catch((error) => {
+        console.error("Ошибка:", error);
       });
   }, []);
 
-
-
   return (
-    <div>
-      <h2>Страница элемента с id: {id}</h2>
+    <div className="page-content">
       {infoList && (
-  <div>
-    <h2>Детали аниме</h2>
-      <RatingComponent animeId={id} />
-      <StatusComponent animeId={id} />
-    <div>
-        <img src={infoList.anime_info2.url_img} alt={infoList.anime_info2.title_ru} />
-        <p>Название на русском: {infoList.anime_info2.title_ru}</p>
-        <p>Название на английском: {infoList.anime_info2.title_en}</p>
-        <p>Тип: {infoList.anime_info2.descriptionEpisod}</p>
-        <p>Дата: {infoList.anime_info2.descriptionData}</p>
-        <p>Оценка: {infoList.anime_info2.score}</p>
-    </div>
-    <h2>Дополнительная информация</h2>
-    <div>
-        <p>Эпизоды: {infoList.anime_info.Episodes}</p>
-        <p>Жанры: {infoList.anime_info.Genres}</p>
-        <p>Темы: {infoList.anime_info.Themes}</p>
-    </div>
-  </div>
-)}
+        <>
+          <h2 className="anime_title">
+            {infoList.anime_info2.title_en} / {infoList.anime_info2.title_en}
+          </h2>
+          <div className="anime-details">
+            <div>
+              <img
+                className="anime-image"
+                src={infoList.anime_info2.url_img}
+                alt={infoList.anime_info2.title_ru}
+              />
+            </div>
+            <div className="infoA"> <h4>Информация</h4> 
+              <p className="anime-description">
+                Тип: {infoList.anime_info2.descriptionEpisod}
+              </p>
+              <p>Эпизоды: {infoList.anime_info.Episodes}</p>
+              <p className="anime-description">
+                Дата: {infoList.anime_info2.descriptionData}
+              </p>
+              <p>Жанры: {infoList.anime_info.Genres}</p>
+              <p>Темы: {infoList.anime_info.Themes}</p>
+            </div>
+            <div className="infoB">
+              <div className="additional-info">Да я вор, Плох?</div>
+              <img className="plox" src={plox} />
+            </div>
+          </div>
+        </>
+      )}
+      <RatingComponent />
+      <StatusComponent />
     </div>
   );
 };
--- frontend/src/components/Contents/SettintContent/RatingComponent.js
+++ frontend/src/components/Contents/SettintContent/RatingComponent.js
@@ -1,25 +1,33 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './RS.css'
+import React, { useState } from "react";
+import axios from "axios";
+import "./RS.css";
 
 const RatingComponent = ({ animeId }) => {
   const [rating, setRating] = useState(0);
 
   const handleRatingChange = async () => {
     try {
-      await axios.post(`http://localhost:8000/api/anime/${animeId}/rating`, { rating });
-      alert('Оценка успешно установлена!');
+      await axios.post(`http://localhost:8000/api/anime/${animeId}/rating`, {
+        rating,
+      });
+      alert("Оценка успешно установлена!");
     } catch (error) {
-      console.error('Ошибка:', error);
+      console.error("Ошибка:", error);
     }
   };
 
   return (
     <div className="rating-component">
       <h3>Установить оценку:</h3>
-      <input type="number" min="1" max="10" value={rating} onChange={(e) => setRating(e.target.value)} />
+      <input
+        type="number"
+        min="1"
+        max="10"
+        value={rating}
+        onChange={(e) => setRating(e.target.value)}
+      />
       <button onClick={handleRatingChange}>Установить</button>
     </div>
   );
 };
-export default RatingComponent
+export default RatingComponent;
